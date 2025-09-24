c0f54e536b33f6b28dee6b9dc9876bb9431d5ab7
Yuhan Zhao <aidenzhao@hotmail.com>
2025-09-24 14:53:43 -0400

    Modified mongodb connection so that it only need connection string

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -4,8 +4,8 @@ declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-const uri = process.env.MONGODB_URI!;
-const dbName = process.env.MONGODB_DB || "storyforge";
+const uri = process.env.DATABASE_URL!;
+// const dbName = process.env.MONGODB_DB || "storyforge";
 
 if (!uri) {
   throw new Error("MONGODB_URI is not defined in .env");
@@ -27,5 +27,5 @@ if (process.env.NODE_ENV === 'development') {
 
 export async function getDb(): Promise<Db> {
   const dbClient = await clientPromise;
-  return dbClient.db(dbName);
+  return dbClient.db('storyforge');
 }
